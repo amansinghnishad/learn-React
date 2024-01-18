291f874bc1be54be2cc202d6d35c1e21fc34e419
Aman Singh Nishad <amansinghnishad0808@gmail.com>
2024-01-18 11:17:32 +0530

    first

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 const heading = React.createElement('h1' , {id:"heading"}, "Hello, world!");
 const react = ReactDOM.createRoot(document.getElementById('react'));
 react.render(heading);
